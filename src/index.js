01cb0697e4c5a1712067b9ad30ca495447bc12bd
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-14 14:42:12 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // console.log('%c HI', 'color: firebrick')
-let dogImages = []
 
 document.addEventListener('DOMContentLoaded', function() {
-    loadImages();
+    // loadImages();
+    loadBreeds();
 });
 
 function loadImages(){
@@ -10,17 +10,48 @@ function loadImages(){
     fetch(imgUrl)
    .then(resp => resp.json())
 //    .then(result => console.log(result.message));
-   .then(function (result) {
-    for (const element of result.message) {
-        addImage(element);
-      };
-       });
+   .then(result => addImage(result) 
+       );
 }
 
-function addImage(image){
-    // grab image container 
+function addImage(result){
+    // grab the div to hold images
     let imageContainer = document.getElementById('dog-image-container')
-    let newImg = document.createElement('img')
-    // add image to container
-    newImg.src = `${image}`;
+        //for (each image) inside of result.message
+        for (const image of result.message) {
+            //create image tags inside iteration to hold each image
+            let newImg = document.createElement('img')
+            //add current iterated url to the new image tag
+            newImg.src = `${image}`;
+            // add completed img tags to imagecontainer
+            //add image tag, and then each src would be the images im iterating
+            imageContainer.appendChild(newImg)
+        }
+}
+
+function loadBreeds(){
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    //    .then(result => console.log(result));
+    .then(result => addBreeds(result.message) 
+    );
+}
+
+function addBreeds(result){
+    let dogList = document.getElementById('dog-breeds')
+    let ul = document.createElement('ul')
+    dogList.appendChild(ul)
+    // iterating with 'in' instead of 'of' because the fetch results were strings and not an object
+    // 'of' is for iterating thru objects and 'in' is for arrays
+        for (const breed in result) {
+            let li = document.createElement('li')
+            li.innerHTML = `${breed}`;
+            ul.appendChild(li)
+            li.addEventListener('click', changeColor);
+        }
+}
+
+function changeColor(event){
+    event.target.style.color = 'red';
 }
